16ed020b00cd6bc7ab554480e7649b06b7d2d2d4
anatolii <batkovich327@gmail.com>
2023-01-08 22:54:20 +0200

    Implement method GET to find article by id

--- src/main/java/com/webblog/blog/Controllers/BlogController.java
+++ src/main/java/com/webblog/blog/Controllers/BlogController.java
@@ -6,8 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.Optional;
 
 @Controller
 public class BlogController {
@@ -16,10 +20,24 @@ public class BlogController {
     private ArticleRepository articleRepository;
 
     @GetMapping("/blog")
-    public String getBlogPage(Model model) {
+    public String getArticles(Model model) {
         Iterable<Article> articles = articleRepository.findAll();
         model.addAttribute("articles", articles);
         return "blog-main";
+    }
+
+    @GetMapping("/blog/{id}")
+    public String getArticle(@PathVariable(value="id") long id, Model model) {
+        if(!articleRepository.existsById(id)) {
+            return "home";
+        }
+
+        Optional<Article> article = articleRepository.findById(id);
+        ArrayList<Article> res = new ArrayList<>();
+        article.ifPresent(res::add);
+        model.addAttribute("article", res);
+
+        return "blog-details";
     }
 
     @GetMapping("/blog/create")
@@ -33,4 +51,6 @@ public class BlogController {
         articleRepository.save(article);
         return "redirect:/blog";
     }
+
+
 }
